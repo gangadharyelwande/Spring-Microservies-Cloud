2e0d783f778e5e585a6a99d701e8601165ea4d26
gangadharyelwande <gangadhar.yelwande@capgemini.com>
2018-09-24 21:03:13 -0400

    NetFlix Zuul First Commit

--- currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyExchangeServiceProxy.java
+++ currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyExchangeServiceProxy.java
@@ -6,13 +6,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 //@FeignClient(name="currency-exchange-service", url="localhost:8000")
-@FeignClient(name="currency-exchange-service")
+//@FeignClient(name="currency-exchange-service")
 @RibbonClient(name="currency-exchange-service")
-//@FeignClient(name="netflix-zuul-api-gateway-server")
+@FeignClient(name="netflix-zuul-api-gateway-server")
 
 public interface CurrencyExchangeServiceProxy {
-	@GetMapping("/currency-exchange/from/{from}/to/{to}")
-	//@GetMapping("/currency-exchange-service/currency-exchange/from/{from}/to/{to}")
+	//@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	@GetMapping("/currency-exchange-service/currency-exchange/from/{from}/to/{to}")
 	public CurrencyConversionBean retrieveExchangeValue
 		(@PathVariable("from") String from, @PathVariable("to") String to);
 }
