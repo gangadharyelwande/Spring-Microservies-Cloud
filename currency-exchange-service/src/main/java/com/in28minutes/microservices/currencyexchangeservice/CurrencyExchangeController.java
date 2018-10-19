df09776035cd35907e28daeade5704d609defe03
gangadharyelwande <gangadhar.yelwande@capgemini.com>
2018-10-19 15:45:48 -0400

    for multiple env files

--- currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -3,7 +3,7 @@ package com.in28minutes.microservices.currencyexchangeservice;
 import java.math.BigDecimal;
 import java.util.logging.Logger;
 
-import org.hibernate.annotations.common.util.impl.LoggerFactory;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
